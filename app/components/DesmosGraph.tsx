1facf910ac266714b805cab82467cf6d03a12b80
Spagestic <steelmax666@gmail.com>
2025-02-20 10:05:35 +0800

    comment out to prevent error on production

--- app/components/DesmosGraph.tsx
+++ app/components/DesmosGraph.tsx
@@ -1,38 +1,38 @@
-import { useEffect, useRef } from "react";
-import Desmos from "desmos";
+// import { useEffect, useRef } from "react";
+// // import * as Desmos from "desmos";
 
-interface DesmosGraphProps {
-  equations: string[];
-}
+// interface DesmosGraphProps {
+//   equations: string[];
+// }
 
-export default function DesmosGraph({ equations }: DesmosGraphProps) {
-  const calculatorRef = useRef<HTMLDivElement>(null);
+// export default function DesmosGraph({ equations }: DesmosGraphProps) {
+//   const calculatorRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (calculatorRef.current) {
-      const calculator = Desmos.GraphingCalculator(
-        calculatorRef.current
-      ) as DesmosConfig;
-      configureDesmos(calculator, equations);
+//   useEffect(() => {
+//     if (calculatorRef.current) {
+//       const calculator = Desmos.GraphingCalculator(
+//         calculatorRef.current
+//       ) as DesmosConfig;
+//       configureDesmos(calculator, equations);
 
-      return () => {
-        calculator.destroy();
-      };
-    }
-  }, [equations]);
+//       return () => {
+//         calculator.destroy();
+//       };
+//     }
+//   }, [equations]);
 
-  return <div ref={calculatorRef} className="w-3/5 min-h-screen" />;
-}
+//   return <div ref={calculatorRef} className="w-3/5 min-h-screen" />;
+// }
 
-export type DesmosConfig = Desmos.GraphingCalculator;
+// export type DesmosConfig = Desmos.GraphingCalculator;
 
-export const configureDesmos = (
-  calculator: DesmosConfig,
-  equations: string[]
-) => {
-  if (Array.isArray(equations)) {
-    equations.forEach((equation, index) => {
-      calculator.setExpression({ id: `graph${index + 1}`, latex: equation });
-    });
-  }
-};
+// export const configureDesmos = (
+//   calculator: DesmosConfig,
+//   equations: string[]
+// ) => {
+//   if (Array.isArray(equations)) {
+//     equations.forEach((equation, index) => {
+//       calculator.setExpression({ id: `graph${index + 1}`, latex: equation });
+//     });
+//   }
+// };
