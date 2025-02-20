118b845326d2c726c32d8c474d608dd9c65183c9
Spagestic <steelmax666@gmail.com>
2025-02-20 09:56:25 +0800

    fix: disable eslint rule for explicit any in utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -5,7 +6,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export const constraintsToEquations = (constraints: any) => {
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   return constraints.map((constraint: any) => {
     const [x1Coefficient, x2Coefficient] = constraint.x;
     const operator = constraint.operator;
